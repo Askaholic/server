26214f8164ab54186156512a5117a31c81a68bd0
Askaholic <askaholic907@gmail.com>
2019-06-04 23:32:04 -0800

    Helpers for running tests with postgres

--- scripts/run_tests_with_coverage.sh
+++ scripts/run_tests_with_coverage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -e
-py.test --cov-report term-missing --cov=server --mysql_database=faf --disable-warnings
+# py.test --cov-report term-missing --cov=server --mysql_database=faf
+py.test --cov-report term-missing --cov=server --mysql_database=sh --mysql_username=postgres --mysql_password=apple --mysql_port=5432 -s
